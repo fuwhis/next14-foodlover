3926dfd58191c957f2be199d8b1e68a5dc1eb526
fuwhis <steve.parker117@gmail.com>
2024-05-10 10:43:52 +0700

    implement loading & fallback while site is loading meals data

--- src/app/meals/page.tsx
+++ src/app/meals/page.tsx
@@ -1,11 +1,19 @@
 import { getMeals } from '@/lib/meals';
 import { IMeal } from '@/types/type';
 import Link from 'next/link';
+import { Suspense } from 'react';
 import MealsGrid from '../components/meals/meals-grid';
+import MealsLoadingPage from '../loading-out';
 import classes from './page.module.css';
 
-export default async function MealPage() {
+const Meals = async () => {
   const meals: Array<IMeal> & any = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+};
+
+export default async function MealPage() {
+
 
   return (
     <>
@@ -22,7 +30,9 @@ export default async function MealPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<MealsLoadingPage />}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
