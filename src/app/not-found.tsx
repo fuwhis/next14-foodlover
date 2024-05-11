9f9527d26dc0283a3a9cc8767f26fd1ce4e6cadb
fuwhis <steve.parker117@gmail.com>
2024-05-11 22:34:16 +0700

    chore!: update not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <main className="not-found">
-      <h1>Not Found</h1>
+      <h1>Meal Not Found</h1>
       <p>Unfortunately, we could not find the request page or resource.</p>
     </main>
   );
