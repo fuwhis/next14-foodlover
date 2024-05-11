b0dd7de9e896cf755909726b4b5736946283a897
fuwhis <steve.parker117@gmail.com>
2024-05-11 22:42:19 +0700

    fix: update type of fetch data function

--- src/lib/meals.ts
+++ src/lib/meals.ts
@@ -1,13 +1,14 @@
+import { IMeal } from '@/types/type';
 import sql from 'better-sqlite3';
 
 const db = sql('meals.db');
 
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
-  return db.prepare('SELECT * FROM meals').all();
+  return db.prepare<unknown[], IMeal>('SELECT * FROM meals').all();
 }
 
 export async function getMealDetailBySlug(slug: string) {
   await new Promise((resolve) => setTimeout(resolve, 2000));
-  return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+  return db.prepare<unknown[], IMeal>('SELECT * FROM meals WHERE slug = ?').get(slug);
 };
