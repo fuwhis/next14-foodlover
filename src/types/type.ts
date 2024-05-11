26ca491abb37a279633828f1de8a118473ee7716
fuwhis <steve.parker117@gmail.com>
2024-05-12 01:36:46 +0700

    Feat: store new recipe in the database

--- src/types/type.ts
+++ src/types/type.ts
@@ -9,8 +9,26 @@ interface IMeal {
   creator_email: string;
 }
 
+interface IImageData {
+  name: string;
+  size: number;
+  type: string;
+  arrayBuffer: () => Promise<ArrayBuffer>;
+}
+
+interface IMealFormData {
+  slug: string;
+  title: string;
+  summary: string;
+  instructions: string;
+  image: IImageData;
+  creator: string;
+  creator_email: string;
+}
+
 interface IProps {
   meals: Array<IMeal>;
 }
 
-export type { IMeal, IProps };
+export type { IImageData, IMeal, IMealFormData, IProps };
+
