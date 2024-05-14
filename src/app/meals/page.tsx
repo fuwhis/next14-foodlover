34e2d6ae14c0a094588c964bfcf64c7a9b064ecb
fuwhis <steve.parker117@gmail.com>
2024-05-14 16:19:08 +0700

    Add: static metadata on page meals

--- src/app/meals/page.tsx
+++ src/app/meals/page.tsx
@@ -1,9 +1,15 @@
 import { getMeals } from '@/lib/meals';
+import { Metadata } from 'next';
 import Link from 'next/link';
 import { Suspense } from 'react';
 import MealsGrid from '../components/meals/meals-grid';
 import MealsLoadingPage from '../loading-out';
 import classes from './page.module.css';
+
+export const metadata: Metadata = {
+  title: "All Meals",
+  description: "Browse the delicious meals shared by our vibrant community.",
+};
 
 const Meals = async () => {
   const meals = await getMeals();
